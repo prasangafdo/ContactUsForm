94b11188bf072a4a8906d6529ca4122d2fae040b
Prasanga Fernando <prasangafdz@outlook.com>
2022-06-17 12:58:48 +0530

    Update contact.us.page.js

--- page/contact.us.page.js
+++ page/contact.us.page.js
@@ -1,6 +1,38 @@
 class ContactUsPage {
+
     get lblContact(){
         return $("//h3[text()='Contact']")
+    }
+    get txtName(){
+        return $('name')
+    }
+    get lblEmail(){
+        return $('email')
+    }
+    get txtPhone(){
+        return $('phone')
+    }
+    get lblWebSite(){
+        return $('url')
+    }
+    get txtMessage(){
+        return $('message')
+    }
+    get btnSubmit(){
+        return('#contact-submit')
+    }
+
+
+
+
+
+
+
+
+
+
+    async navigateToContactForm(){
+        await browser.url('https://contact-prasanga.000webhostapp.com/form.php') //parameterize this later
     }
 }
 
