daf38a293bd27b0d9d524849d68333f12548a8c6
Prasanga Fernando <prasangafdz@outlook.com>
2022-06-20 12:52:47 +0530

    Updated 'Verify the user can send a message via contact us form'

--- test/contactUsTest.js
+++ test/contactUsTest.js
@@ -8,6 +8,11 @@ describe('Verify the functionalities of the contact us form', ()=>{
     it('Verify the user can send a message via contact us form', async ()=>{
         await contact.navigateToContactForm()
         expect(await contact.isContactUsTopicDisplaying()).toBeTruthy()
-        
+        await contact.enterName('Test name1')
+        await contact.enterEmail('test@abc.com')
+        await contact.enterPhone('0123456789')
+        await contact.enterWebSite('https://www.google.com/')
+        await contact.enterMessage('Test message')
+        await contact.clickSubmitButton()
     })
 })
